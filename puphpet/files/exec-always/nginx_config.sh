96adc181a9b0811d1f9032b01844922b93855fa2
thatcheck <admin.bananaweb@gmail.com>
2015-06-04 14:28:09 +0200

    first stable revision

--- puphpet/files/exec-always/nginx_config.sh
+++ puphpet/files/exec-always/nginx_config.sh
@@ -1,6 +1,6 @@
 # config nginx for mean
-rm -v /etc/nginx/sites-available/*
-rm -v /etc/nginx/sites-enabled/*
-cp -v '/vagrant/puphpet/files/misc/nginx/nginx_config.config' '/etc/nginx/sites-available/mean.dev.conf'
-cp -v '/vagrant/puphpet/files/misc/nginx/nginx_config.config' '/etc/nginx/sites-enabled/mean.dev.conf'
+rm -r /etc/nginx/sites-available/*
+rm -r /etc/nginx/sites-enabled/*
+cp -v '/vagrant/puphpet/files/misc/nginx/nginx_config.conf' '/etc/nginx/sites-available/mean.dev.conf'
+cp -v '/vagrant/puphpet/files/misc/nginx/nginx_config.conf' '/etc/nginx/sites-enabled/mean.dev.conf'
 service nginx restart
